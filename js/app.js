455b3d603f7b11f8bb640c036dbc51e8601aa530
Samrit Basnet <samritbasnet70@gmail.com>
2025-08-10 10:32:03 -0400

    feat(js) adding interactivty with forms

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const taskForm = document.getElementById("taskForm");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+let tasks = [];
+
+function init() {
+  console.log("Task Manager initialized!");
+}
+
+document.addEventListener("DOMContentLoaded", init);
